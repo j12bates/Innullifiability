49c1b86ff34381da939b72d49844cf664a0b7978
Jacob Bates <jopb1184@gmail.com>
2023-04-01 20:05:28 -0700

    Basic Allocate/Deallocate Functions
    Yeah. Just allocation and deallocation. Nothing too fancy. Just an M
    Choose N function. Storing a list of factorial numbers doesn't work for
    M > 20, but this should.

--- setRec.c
+++ setRec.c
@@ -24,10 +24,6 @@ struct Base {
 // Other Typedefs
 typedef enum SR_QueryMode QueryMode;
 
-// Array of Factorials
-unsigned long long *fact = NULL;
-size_t factc = 0;
-
 // Helper Function Declarations
 
 // ============ User-Level Functions
@@ -41,12 +37,34 @@ size_t factc = 0;
 // Returns NULL on memory or input error
 Base *sr_initialize(size_t size, unsigned long max)
 {
-    return NULL;
+    // We can't have more elements than possible values
+    if (max < levels) return NULL;
+
+    // Allocate Memory for Information Structure
+    Base *base = malloc(sizeof(Base));
+    if (base == NULL) return NULL;
+
+    // Populate Information Structure
+    base->size = size;
+    base->max = max;
+
+    // Allocate Memory for Record Array
+    Rec *rec = calloc(mcn(max, size), sizeof(Rec));
+    if (rec == NULL) return NULL;
+    base->rec = rec;
+
+    return base;
 }
 
 // Release a Set Record
 void sr_release(Base *base)
 {
+    // Free the Record Array
+    free(base->rec);
+
+    // Free the Information Structure itself
+    free(base);
+
     return;
 }
 
@@ -70,3 +88,23 @@ long long sr_query(const Base *base, QueryMode,
 
 // These functions are helper functions for the main user-level
 // functions.
+
+// M Choose N
+unsigned long long mcn(size_t m, size_t n)
+{
+    // Invalid Case
+    if (m < n) return 0;
+
+    // Total Ordered Combinations, Permutations
+    unsigned long long total = 1, perms = 1;
+    for (size_t i = 0; i < m - n; i++)
+    {
+        total *= m - i;
+        perms *= i + 1;
+    }
+
+    // Don't divide by zero
+    if (perms == 0) return 0;
+
+    return total / perms;
+}
