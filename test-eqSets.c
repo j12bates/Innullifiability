bd015645354e29083b937aa74596134ed998477d
Jacob Bates <jopb1184@gmail.com>
2023-04-23 00:42:02 -0700

    Updated Equivalent Sets Test Program
    Now the test program should compile. The only real thing is the new
    parameters for the init function.

--- test-eqSets.c
+++ test-eqSets.c
@@ -4,33 +4,43 @@
 
 #include "eqSets.h"
 
+void expandPrint(const unsigned long *, size_t);
 void printSet(const unsigned long *, size_t);
 
 int main(int argc, char *argv[])
 {
-    eqSetsInit(3, 6);
-    printf("Initializing with N = 3, M = 6\n\n");
+    eqSetsInit(6);
+    printf("Initialized with M = 6\n\n");
 
     unsigned long nullThree[2] = {3, 3};
     eqSets(nullThree, 2, &printSet);
-    printf("\nSets equivalent to (3,3)\n\n");
+    printf("\nExpanded from (3,3)\n\n");
 
     unsigned long nullTwo[2] = {2, 2};
     eqSets(nullTwo, 2, &printSet);
-    printf("\nSets equivalent to (2,2)\n\n");
+    printf("\nExpanded from (2,2)\n\n");
 
     unsigned long oneFour[2] = {1, 4};
     eqSets(oneFour, 2, &printSet);
-    printf("\nSets equivalent to (1,4)\n\n");
+    printf("\nExpanded from (1,4)\n\n");
 
     unsigned long justTwo[1] = {2};
     eqSets(justTwo, 1, &printSet);
-    printf("\nSets equivalent to (2)\n\n");
+    printf("\nExpanded from (2)\n\n");
 
-    eqSetsInit(0, 0);
+    eqSets(justTwo, 1, &expandPrint);
+    printf("\nExpanded from above expansions of (2)\n\n");
+
+    eqSetsInit(0);
     printf("Freeing Memory\n");
 
     return 0;
+}
+
+void expandPrint(const unsigned long *set, size_t setc)
+{
+    eqSets(set, setc, &printSet);
+    return;
 }
 
 void printSet(const unsigned long *set, size_t setc)
