6a19259741e3150579f1dd935da5611ce1d138a4
Jacob Bates <jopb1184@gmail.com>
2023-07-12 22:19:58 -0700

    Generation - Arguments and Record Importing
    Handling command-line arguments as well as importing source and
    destination records. I want to do error checking for the imports, but
    it's a few cases for each, and I don't like the idea of copy-pasting it.
    I'm thinking of having all the error stuff use a goto to the end of the
    Main function, and implementing the result checking as either a separate
    macro or function. So far this is program running as expected.

--- generation.c
+++ generation.c
@@ -11,6 +11,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <errno.h>
+
 #include "setRec/setRec.h"
 #include "mutate/mutate.h"
 #include "supers/supers.h"
@@ -23,10 +25,83 @@
 unsigned int threads = 1;
 
 // Set Records
-SR_Base *src, *dest;
+SR_Base *src = NULL;
+SR_Base *dest = NULL;
 size_t srcSize;
+char *srcFname, *destFname;
 
 int main(int argc, char **argv)
 {
+    // Result Storage from Function Calls
+    int res;
+
+    // ============ Command-Line Arguments
+
+    // Usage Check
+    if (argc < 4) {
+        fprintf(stderr, "Usage: %s %s %s %s %s\n",
+                argv[0], "srcSize", "src.dat", "dest.dat", "[threads]");
+        return 1;
+    }
+
+    // Numeric Arguments
+    errno = 0;
+    srcSize = strtoul(argv[1], NULL, 10);
+    if (errno) {
+        perror("srcSize argument");
+        return 1;
+    }
+
+    // Record Filenames
+    srcFname = argv[2];
+    destFname = argv[3];
+
+    // ============ Import Records
+
+    // Initialize Records
+    src = sr_initialize(srcSize);
+    dest = sr_initialize(srcSize + 1);
+
+    // Deal with Files
+    {
+        FILE *f;
+
+        // Open Source Record File
+        f = fopen(srcFname, "rb");
+        if (f == NULL) {
+            perror("src Import");
+            return 1;
+        }
+
+        // Import if Successful
+        else {
+            res = sr_import(src, f);
+            if (res); // some error checking; gah, I can't figure it out
+
+            fclose(f);
+        }
+
+        // Open Destination Record File
+        f = fopen(destFname, "rb");
+        if (f == NULL) {
+            perror("dest Import");
+            return 1;
+        }
+
+        // Import if Successful
+        else {
+            res = sr_import(dest, f);
+            if (res); // same as above
+
+            fclose(f);
+        }
+    }
+
+    // Record Information
+    printf("src  - Size: %2zu; M: %4lu to %4lu\n",
+            sr_getSize(src), sr_getMinM(src), sr_getMaxM(src));
+    printf("dest - Size: %2zu; M: %4lu to %4lu\n",
+            sr_getSize(dest), sr_getMinM(dest), sr_getMaxM(dest));
+
     return 0;
 }
