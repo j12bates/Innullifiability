ee66db90810536119659192e284c702778f32e23
Jacob Bates <jopb1184@gmail.com>
2023-07-18 08:59:49 -0700

    Weed - Arguments, Import/Export
    Implemented reading in arguments, importing the record, and exporting
    the record. Just need to make the modified thread function, the verify
    function, and create the threads.

--- weed.c
+++ weed.c
@@ -11,6 +11,7 @@
 #include <errno.h>
 #include <pthread.h>
 
+#include "setRec/setRec.h"
 #include "nulTest/nulTest.h"
 
 #include "common.c"
@@ -27,5 +28,55 @@ char *fname;
 
 int main(int argc, char **argv)
 {
+    // ============ Command-Line Arguments
+
+    // Usage Check
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s %s %s %s\n",
+                argv[0], "recSize", "rec.dat", "[threads]");
+        return 1;
+    }
+
+    // RecSize Argument
+    errno = 0;
+    size = strtoul(argv[1], NULL, 10);
+    if (errno) {
+        perror("recSize Argument");
+        return 1;
+    }
+
+    // Optional Threads Argument
+    errno = 0;
+    if (argc > 4) {
+        threads = strtoul(argv[3], NULL, 10);
+        if (errno) {
+            perror("threads argument");
+            return 1;
+        }
+    }
+
+    // Record Filename
+    fname = argv[2];
+
+    // ============ Import Record
+    rec = sr_initialize(size);
+
+    {
+        int openImport(SR_Base *, char *);
+
+        fprintf(stderr, "Importing Record...");
+        if (openImport(rec, fname)) return 1;
+    }
+
+    // ============ Export and Cleanup
+    {
+        int openExport(SR_Base *, char *);
+
+        fprintf(stderr, "Exporting Record...");
+        if (openExport(rec, fname)) return 1;
+    }
+
+    sr_release(rec);
+
     return 0;
 }
