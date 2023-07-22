59e3e5ff9594de214f4fc9656f42fbbb64f1215c
Jacob Bates <jopb1184@gmail.com>
2023-07-21 19:31:43 -0700

    Command-Line Argument Parsing Function
    I've written a general function for parsing command-line arguments for
    any utility program. It's a variadic function, and it takes in an array
    of symbols to determine each parameter type, as well as a count of
    required parameters, the arguments themselves, and then continually
    takes references to variables in which to store the processed arguments.
    
    Right now, it doesn't handle options, but I'm thinking of implementing
    that by having it accept a mutable string, and replace characters with
    '\0' if they weren't given as an option. I can start using this function
    in programs in the state it's in, and implement options later.

--- common.c
+++ common.c
@@ -54,3 +54,75 @@ int openExport(SR_Base *rec, char *fname)
 
     return 0;
 }
+
+// Parse Command-Line Arguments
+// Returns 0 on success, 1 on invalid arguments
+
+// Takes a null-terminated array of symbols that represent argument
+// formats, the number of required arguments, the command-line arguments
+// themselves, and then references to all the variables to store each
+// argument to. Each variable will be updated with the value of the
+// argument, unless the argument was not given. Undefined behaviour on
+// invalid arguments.
+int argParse(const Param *params, int reqd,
+        int argc, char **argv, ...)
+{
+    va_list ap;
+
+    // If not enough arguments, invalid usage
+    if (argc - 1 < reqd) return 1;
+
+    // Go Through Arguments
+    va_start(ap, argv);
+    for (int i = 1; i < argc; i++)
+        switch (params[i - 1])
+    {
+        char *endptr;
+
+    // For numeric parameters, interpret argument as an integer, check
+    // for errors
+    case PARAM_VAL:
+        {
+            unsigned long *p = va_arg(ap, unsigned long *);
+            *p = strtoul(argv[i], &endptr, 0);
+        }
+        goto errCk;
+
+    case PARAM_CT:
+    case PARAM_SIZE:
+        {
+            size_t *p = va_arg(ap, size_t *);
+            *p = strtoul(argv[i], &endptr, 0);
+        }
+
+    errCk:
+        if (*endptr != '\0') errno = EINVAL;
+        if (errno) {
+            fprintf(stderr, "argv[%d] ", i);
+            perror("Validation");
+            goto invalid;
+        }
+        break;
+
+    // For string parameters, just point to the argument
+    case PARAM_FNAME:
+    case PARAM_STR:
+        {
+            char **p = va_arg(ap, char **);
+            *p = argv[i];
+        }
+        break;
+
+    // If we hit the end, invalid usage
+    default:
+        goto invalid;
+    }
+
+end:
+    va_end(ap);
+    return 0;
+
+invalid:
+    va_end(ap);
+    return 1;
+}
--- common.h
+++ common.h
@@ -6,3 +6,13 @@
 #define NULLIF 1 << 0
 #define ONLY_SUP 1 << 1
 #define MARKED NULLIF | ONLY_SUP
+
+typedef enum ParamType Param;
+enum ParamType {
+    PARAM_END = 0,
+    PARAM_CT,
+    PARAM_SIZE,
+    PARAM_VAL,
+    PARAM_FNAME,
+    PARAM_STR
+};
