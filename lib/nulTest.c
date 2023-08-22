206e895a7daa6b425f87a60aa42a3990f7c084d4
Jacob Bates <jopb1184@gmail.com>
2023-08-21 18:54:17 -0700

    Exhaustive Test - Optimized Length-3
    Now the recursive function bases out at length-3 sets, and a separate
    function takes it from there. The function does a simple equality check,
    and then tries out operations to see if it can make two values into the
    remaining one. At first, I thought I had to do this in a for-loop to get
    all the different possible arrangements, but as it turns out, I don't
    have to, as anything can be rearranged by inverse operations to just the
    original possibilities. How cool is that? I wonder if I could use that
    principle to optimize the recursive logic. But anyways, this new
    function is vastly simpler and will return a result much faster than
    going back through the recursive function's logic.
    
    Then I made another simplification: the user-mode function now also
    ensures there are no zeros, so the other logic doesn't have to deal with
    that. It'd be redundant to check if there are equal values every time
    but then also check for zeros, as they can only come from equal values.
    
    The N = 6, M <= 40 case now only takes 0.79s to run, and the weeding
    phase of that has been reduced to 0.120s. As for the N = 7, M <= 81 case
    (which, as I should have clarified earlier, was also just the weeding
    phase), it went down to 25s. Also, it turns out disk I/O isn't included
    in user time, meaning my measurements from before are also good. Holy
    mackerel, this is so much faster than before!

--- lib/nulTest.c
+++ lib/nulTest.c
@@ -3,12 +3,11 @@
 // Copyright (c) 2023, Jacob Bates
 // SPDX-License-Identifier: BSD-2-Clause
 
-// This code, honestly, is pretty bad. It works, but I know it could be
-// so much better. I just have no idea how to optimize it. This will be
-// replaced at some point, and I feel like it'll probably end up being
-// done in a high-level language like Haskell or something. This is just
-// so slow. For goodness sake, adding in a base case for zero-length
-// increases runtime by like 20-30%.
+// This will be replaced at some point, and I feel like it'll probably
+// end up being done in a high-level language like Haskell or something.
+// This is just kinda hard to reason about. I do know that many more
+// frames exist for the smaller sets than the larger ones, so my idea
+// right now is just to work on optimizing for those smaller sets.
 
 #include <stdlib.h>
 #include <stdbool.h>
@@ -21,10 +20,43 @@ int nulTest(const unsigned long *set, size_t setc)
 
     // Simple cases to not use recursion on
     if (setc == 0) return 1;
-    if (setc == 1) return !!set[0];
+    if (setc == 1) return set[0] != 0;
+    if (setc == 2) return set[0] != set[1];
+    for (size_t i = 0; i < setc; i++) if (set[i] == 0) return 0;
 
     // Use recursion
     return recursiveTest(set, setc);
+}
+
+// Test if a Length-3 Set is Nullifiable or Not
+// Returns 0 if nullifiable, 1 if innullifiable
+
+// This function is simplified and optimized for length-3 sets. It first
+// checks if any two values are equal. Then it checks if applying some
+// operation to two values will result in a matching pair. It can do so
+// using only six checks and two operators since the pairs of arithmetic
+// operations are inverse and all the other possibilities can just be
+// rearranged into one of these. Like the recursive function, this only
+// takes positive integers.
+int nulTestTriplet(const unsigned long set[3])
+{
+    unsigned long a = set[0], b = set[1], c = set[2];
+
+    // Try simple equality
+    if (a == b || b == c || c == a) return 0;
+
+    // Try additive operations
+    else if (a + b == c) return 0;
+    else if (b + c == a) return 0;
+    else if (c + a == b) return 0;
+
+    // Try multiplicative operations
+    else if (a * b == c) return 0;
+    else if (b * c == a) return 0;
+    else if (c * a == b) return 0;
+
+    // If none worked, innullifiable
+    return 1;
 }
 
 // Returns 0 if nullifiable, 1 if innullifiable, -1 on memory error
@@ -37,26 +69,16 @@ int nulTest(const unsigned long *set, size_t setc)
 // nullifiable at any point, that means the set was always nullifiable.
 // A set is only innullifiable if the test always returns that result
 // after every operation. This function only works on sets that are
-// size-2 or larger.
+// size-3 or larger, and positive integers only.
 int recursiveTest(const unsigned long *set, size_t setc)
 {
     // Base case
-    if (setc == 2) return set[0] != set[1];
+    if (setc == 3) return nulTestTriplet(set);
 
-    // Here we're going to pass over everything once, just to check if
-    // we can immediately say this set is nullifiable without doing a
-    // bunch of hard work
-
-    // Iterate over every value
+    // Pass over every pair of values, checking for simple equality
     for (size_t pairA = 0; pairA < setc; pairA++)
-    {
-        // If this element is a zero, easy
-        if (set[pairA] == 0) return 0;
-
-        // If it's equal to any later element, easy
         for (size_t pairB = pairA + 1; pairB < setc; pairB++)
             if (set[pairA] == set[pairB]) return 0;
-    }
 
     // If we can't prove nullifiability in this state, we're gonna have
     // to do some arithmetic and change up how the set looks. We'll try
@@ -90,7 +112,8 @@ int recursiveTest(const unsigned long *set, size_t setc)
             replacements[0] = a + b;
             replacements[1] = a * b;
 
-            // There is one difference
+            // There is one difference (won't generate a zero as we've
+            // already scanned for equality)
             if (a > b) replacements[2] = a - b;
             else replacements[2] = b - a;
 
