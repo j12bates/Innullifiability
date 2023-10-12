6dce63ec076799213839c37813435ab5946830ac
Jacob Bates <jopb1184@gmail.com>
2023-10-11 22:38:28 -0700

    Expand - Validation, Supersets
    Implemented a test for input set validation. Also went and basically
    copied the existing Superset enumeration function, which works by just
    iterating through the potential insertion values, advancing to the next
    set element when it reaches that value.
    
    I'm thinking maybe I could do something like that for inserting
    equivalent pairs too, at least for the additives (and when I say that I
    mean both addition and subtraction), since they are nice and ordered,
    and it'd just be a case of having there be two running indices, if that
    makes sense. For the multiplicatives I may just have it manually insert
    stuff.

--- lib/expand.c
+++ lib/expand.c
@@ -11,11 +11,61 @@
 
 #include <stdlib.h>
 
+// Helper Function Declarations
+int supers(const unsigned long *, size_t, unsigned long,
+        void (*out)(const unsigned long *, size_t));
+
 // Produce All Set Expansions
 // Returns 0 on success, -1 on error (check errno)
 int expand(const unsigned long *set, size_t size,
         unsigned long max, int mode,
         void (*out)(const unsigned long *, size_t))
 {
+#ifndef NO_VALIDATE
+    // Validate Input Set: values are ascending and in-range
+    errno = EINVAL;
+    if (set[0] < 1) return -1;
+    for (size_t i = 1; i < size; i++)
+        if (set[i] <= set[i - 1]) return -1;
+    if (set[setc - 1] > max) return -1;
+    errno = 0;
+#endif
+
+    return 0;
+}
+
+// ============ Helper Functions
+
+// Enumerate Supersets
+// Returns 0 on success, -1 on error (check errno)
+int supers(const unsigned long *set, size_t size, unsigned long max,
+        void (*out)(const unsigned long *, size_t))
+{
+    // Set Representation
+    unsigned long *super = calloc(size + 1, sizeof(unsigned long));
+    if (super == NULL) return -1;
+
+    // Initialize with the input, leaving a spot for insertion
+    for (size_t i = 0; i < size; i++) super[i + 1] = set[i];
+
+    // Iterate over values to insert, keeping track of index
+    size_t pos = 0;
+    for (size_t i = 1; i <= max; i++)
+    {
+        // Insert Value
+        super[pos] = i;
+
+        // If we've reached the next value, skip and advance
+        if (pos < size) if (super[pos + 1] == i) {
+            pos++;
+            continue;
+        }
+
+        // Otherwise, we have a superset to output
+        if (out != NULL) out(super, size + 1);
+    }
+
+    free(super);
+
     return 0;
 }
