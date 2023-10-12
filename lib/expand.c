823a56b0dbfa7d9a2906e83fa4ec1f0d77b08e5b
Jacob Bates <jopb1184@gmail.com>
2023-10-12 15:54:27 -0700

    Expand - Basic Mutation, Patched Issues
    Here's what I'm thinking for mutations in the new Expand program. The
    logic for additive mutations will be just like the superset expansion,
    just with two insertions to keep track of. I've implemented this for
    addition, and with just a few modifications it's just as good for
    subtraction as well. There's a more high-level mutation function that'll
    use this as well as whatever solution I make for multiplication and
    division, probably just raw insertion.
    
    Apart from that, I fixed a few compile errors and inconsistencies.
    Nothing really to test yet, but soon I'll work on a test program.

--- lib/expand.c
+++ lib/expand.c
@@ -10,10 +10,17 @@
 // output through a function pointer.
 
 #include <stdlib.h>
+#include <stdbool.h>
+
+#include <errno.h>
 
 // Helper Function Declarations
 int supers(const unsigned long *, size_t, unsigned long,
-        void (*out)(const unsigned long *, size_t));
+        void (*)(const unsigned long *, size_t));
+int mutateAdd(const unsigned long *, size_t, unsigned long,
+        void (*)(const unsigned long *, size_t));
+void mutateOpSum(unsigned long *, size_t, size_t, unsigned long,
+        void (*)(const unsigned long *, size_t));
 
 // Produce All Set Expansions
 // Returns 0 on success, -1 on error (check errno)
@@ -27,7 +34,7 @@ int expand(const unsigned long *set, size_t size,
     if (set[0] < 1) return -1;
     for (size_t i = 1; i < size; i++)
         if (set[i] <= set[i - 1]) return -1;
-    if (set[setc - 1] > max) return -1;
+    if (set[size - 1] > max) return -1;
     errno = 0;
 #endif
 
@@ -50,7 +57,7 @@ int supers(const unsigned long *set, size_t size, unsigned long max,
 
     // Iterate over values to insert, keeping track of index
     size_t pos = 0;
-    for (size_t i = 1; i <= max; i++)
+    for (unsigned long i = 1; i <= max; i++)
     {
         // Insert Value
         super[pos] = i;
@@ -68,4 +75,72 @@ int supers(const unsigned long *set, size_t size, unsigned long max,
     free(super);
 
     return 0;
+}
+
+// Enumerate Additively Mutated Sets
+// Returns 0 on success, -1 on error (check errno)
+int mutateAdd(const unsigned long *set, size_t size, unsigned long max,
+        void (*out)(const unsigned long *, size_t))
+{
+    // Set Representation for Expanded Set
+    unsigned long *eSet = calloc(size + 1, sizeof(unsigned long));
+    if (eSet == NULL) return -1;
+
+    // Iterate through all the different elements we could mutate
+    for (size_t mutPt = 0; mutPt < size; mutPt++)
+    {
+        // Initialize output and call helper function (Sum)
+        for (size_t i = 0; i < size; i++) eSet[i + 1] = set[i];
+        mutateOpSum(eSet, size + 1, mutPt, max, out);
+
+        // do something similar for difference
+    }
+
+    return 0;
+}
+
+// This takes in a set with an unused spot at the beginning, as well as
+// an index in that set whose value to mutate, and it inserts all the
+// equivalent pairs it can using addition, outputting all of these
+// expanded sets.
+
+// This works kinda like the superset expansion function, except it has
+// two insertions, a main and a follower. The main value is always
+// ahead, starting out at the value we're actually mutating, and the
+// main plus the follower is always that value. And similarly the
+// indices of insertion will shift whenever the values collide.
+void mutateOpSum(unsigned long *eSet, size_t eSize,
+        size_t mutPt, unsigned long max,
+        void (*out)(const unsigned long *, size_t))
+{
+    // Value we're mutating
+    unsigned long mutVal = eSet[mutPt];
+
+    // Iterate through all the main values we could insert, keeping
+    // track of the indices we're inserting at
+    size_t insPtMain = mutPt, insPtFollower = 0;
+    for (unsigned long insValMain = mutVal - 1;
+            insValMain > mutVal / 2; insValMain--)
+    {
+        // Follower insertion value
+        unsigned long insValFollower = mutVal - insValMain;
+
+        // Insert Values
+        eSet[insPtMain] = insValMain;
+        eSet[insPtFollower] = insValFollower;
+
+        // If either of these insertions have reached the value ahead of
+        // it, advance its index, and skip to the next equivalent pair
+        // since this has a double value
+        bool skip = false;
+        if (eSet[insPtMain - 1] == insValMain)
+            insPtMain--, skip = true;
+        if (eSet[insPtFollower + 1] == insValFollower)
+            insPtFollower++, skip = true;
+
+        // Otherwise, we have an expanded set to output
+        if (out != NULL && !skip) out(eSet, eSize);
+    }
+
+    return;
 }
