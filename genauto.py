4a3bef31798f3d54cae783f641513e64c446ebe3
Jacob Bates <jopb1184@gmail.com>
2024-03-25 23:12:56 -0700

    Database Script - Successor Range Chooser
    Created a function that figures out the best successor range given the
    previously generated one. Basically it will take in the set size, last
    M-value, and fixed values, and spit out a new M-value range and new (or
    the same) fixed values. It tries to make as large a record as possible
    while keeping to a size restriction.
    
    I need to try and document it better, like by making examples. Want to
    show how the recursion helps with collapsing sequential fixed values, as
    well as why the conditions are in this order.

--- genauto.py
+++ genauto.py
@@ -4,8 +4,39 @@ import math
 import os
 import sys
 
-def recSize(N, minM, maxM):
-    return math.comb(maxM, N) - math.comb(minM - 1, N)
+def recSize(N, minM, maxM, fixed):
+    k = N - len(fixed)
+    return math.comb(maxM, k) - math.comb(max(0, minM - 1), k)
+
+# returns (N, minM, maxM, fixed)
+# This will take in the range info from the previous record, and figure
+# out a successor range, as large as possible while keeping to a size
+# restriction.
+def nextRange(N, lastM, lastFixed, limitM, maxRecSize):
+    nextFixed = lastFixed
+    nextMinM = lastM + 1
+
+# if this fixed value has run its course, treat it like our last M-value
+    if len(lastFixed) != 0:
+        if nextMinM == lastFixed[0]:
+            nextFixed = nextFixed[1:]
+            return nextRange(N, nextMinM, nextFixed, limitM, maxRecSize)
+
+# go into a new fixed value if there's not enough space for a whole new
+# M-value
+    if recSize(N, nextMinM, nextMinM, nextFixed) > maxRecSize:
+        nextFixed = [nextMinM] + lastFixed
+        nextMinM = 0
+
+# enumerate max M-values until we'd exceed the size limit
+    nextMaxM = nextMinM
+    for i in range(nextMinM, (nextFixed + [limitM])[0]):
+        if recSize(N, nextMinM, i, nextFixed) <= maxRecSize:
+            nextMaxM = i
+        else:
+            break
+
+    return (N, nextMinM, nextMaxM, nextFixed)
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
