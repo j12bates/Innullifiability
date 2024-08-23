febc23f258a8dc44538563e9be006830020fad13
Jacob Bates <jopb1184@gmail.com>
2024-08-23 12:22:31 -0700

    Exhaustive Test - No Wrap-Around, Equality Checks
    In testing the N = 8 sets, I'm getting worried that there's potential
    for wrap-around causing false negatives. If the test multiplies together
    a lot of positive values, it could coincidentally line up with some
    other values in the set and cause a zero to appear. Realistically
    though, with the values I'm working with, it couldn't cause a false
    positive. And it could be that even in principle that'd be impossible
    due to the nature of wrap-around being well-defined... but I gotta do
    some thinking on that. So what I've done here is simply disable using a
    value that's wrapped around from multiplication. I guess technically now
    the handful of results need to be tested by something else that can
    handle large integers, though this doesn't matter for my purposes, as I
    mentioned.
    
    Also, I made what I think is an optimization on the checks for equality.
    I'm doing them now right before any recursion takes place, potentially
    saving lots of calls when there's already a computed zero. This means
    the function in general doesn't take sets with repeated values. Also
    enabled GCC optimization level 3, because I might as well. It should
    speed things up. Once I've verified this new change works and is
    correct, I'll deploy it and redo one of my expansions to check for
    differences. I'd be surprised if there was anything different.
    
    Since I've developed my system of nineteen formulations to check a
    4-set, I think I might as well work on a replacement for this program.
    I'll probably write it in Haskell as it's good for recursion and such,
    and it has arbitrary-length integers. I also think I can manage to do
    something similar for N = 5. I'd be interested to see how much runtime
    these optimizations will save. Each of N = 2 and N = 3 saw a 5-6x speed-
    up.

--- lib/nulTest.c
+++ lib/nulTest.c
@@ -11,6 +11,7 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
+#include <limits.h>
 
 // Test if a set is Nullifiable or Not
 // Returns 0 if nullifiable, 1 if innullifiable, -1 on memory error
@@ -33,22 +34,17 @@ int nulTest(const unsigned long *set, size_t size,
 // Test if a Length-3 Set is Nullifiable or Not
 // Returns 0 if nullifiable, 1 if innullifiable
 
-// This function is simplified and optimized for length-3 sets. It first
-// checks if any two values are equal. Then it checks if applying some
-// operation to two values will result in a matching pair. It can do so
-// using only six checks and two operators since the pairs of arithmetic
+// This function is simplified and optimized for length-3 sets. It uses
+// only six checks and two operators since the pairs of arithmetic
 // operations are inverse and all the other possibilities can just be
 // rearranged into one of these. Like the recursive function, this only
-// takes positive integers.
+// takes sets of positive integers with no double-values.
 int nulTestTriplet(const unsigned long set[3])
 {
     unsigned long a = set[0], b = set[1], c = set[2];
 
-    // Try simple equality
-    if (a == b || b == c || c == a) return 0;
-
     // Try additive operations
-    else if (a + b == c) return 0;
+    if (a + b == c) return 0;
     else if (b + c == a) return 0;
     else if (c + a == b) return 0;
 
@@ -65,32 +61,27 @@ int nulTestTriplet(const unsigned long set[3])
 
 // This function will simply determine whether or not a set is
 // nullifiable, given an initial reduction space. It is a recursive
-// function, and it starts by checking if there is a trivial way to
-// nullify the set, and then it performs every single possible operation
-// on the set, before recursing and checking again as though that were a
-// new set. The initial reduction space gives the M-range that this 'new
-// set' must fit in for it to go through the recursion. If it finds that
-// the set is nullifiable at any point, that means the set was always
-// nullifiable. A set is only innullifiable if the test always returns
-// that result after every operation. This function only works on sets
-// that are size-3 or larger, and positive integers only. The maximum
-// M-value can be set to zero to indicate no upper bound.
+// function, and it performs every single possible operation on the set,
+// reducing it, before recursing and checking again as though that were
+// a new set. The initial reduction space gives the M-range that this
+// 'new set' must fit in for it to go through the recursion. If it finds
+// that the set is nullifiable at any point, that means the set was
+// always nullifiable. A set is only innullifiable if the test always
+// returns that result after every operation. This function only works
+// on sets that are size-3 or larger, containing positive values only,
+// and who have no double-values. The maximum M-value can be set to zero
+// to indicate no upper bound. Also, this does not check the cases where
+// there was wrap-around, so technically any innullifiable outputs have
+// to undergo a check that can handle large integers just to be sure.
 int recursiveTest(const unsigned long *set, size_t size,
         unsigned long minm, unsigned long maxm)
 {
     // Base case
     if (size == 3) return nulTestTriplet(set);
 
-    // Pass over every pair of values, checking for simple equality
-    for (size_t pairA = 0; pairA < size; pairA++)
-        for (size_t pairB = pairA + 1; pairB < size; pairB++)
-            if (set[pairA] == set[pairB]) return 0;
-
-    // If we can't prove nullifiability in this state, we're gonna have
-    // to do some arithmetic and change up how the set looks. We'll try
-    // every arithmetic operation we can on every pair we can, and pass
-    // in that new 'set' to ourselves. If we can show nullifiability at
-    // any point, that carries on
+    // Try every arithmetic operation we can on every pair we can, and
+    // pass in that new 'set' to ourselves. If we can show
+    // nullifiability at any point, that carries on
 
     // Allocate Space for New Set
     unsigned long *newSet = calloc(size - 1, sizeof(unsigned long));
@@ -122,7 +113,6 @@ int recursiveTest(const unsigned long *set, size_t size,
         // arithmetic operations on them, or replacement values
         unsigned long replacements[4] = {0};
         replacements[1] = a + b;
-        replacements[3] = a * b;
 
         // There is one difference (won't generate a zero as we've
         // already scanned for equality)
@@ -132,6 +122,16 @@ int recursiveTest(const unsigned long *set, size_t size,
         // Up to one quotient is possible
         if (a % b == 0) replacements[2] = a / b;
         else if (b % a == 0) replacements[2] = b / a;
+
+        // Don't bother with the product if it'll wrap around
+        if (a <= UINT_MAX / b) replacements[3] = a * b;
+
+        // If any of these equal any of the existing values, that's a
+        // double-value and a zero
+        for (size_t i = 0; i < 4; i++)
+            for (size_t index = 1; index < size - 1; index++)
+                if (replacements[i] == newSet[index])
+                    return 0;
 
         // Iterate through those replacement values
         for (size_t i = 0; i < 4; i++)
